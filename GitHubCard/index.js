f25c00835e9d40cb3b4d6bc574a853c9851eca74
unknown <ken.brandon@aol.com>
2020-01-23 17:24:22 -0700

    mvp

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,3 +1,7 @@
+const cardsAttach = document.querySelector(".cards");
+
+console.log(cardsAttach);
+
 /* Step 1: using axios, send a GET request to the following URL 
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
@@ -6,9 +10,20 @@
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
-
    Skip to Step 3.
 */
+
+axios
+  .get("https://api.github.com/users/Kenneth-Brandon")
+  .then(response => {
+    const gitData = response.data;
+
+    cardsAttach.appendChild(createUserCard(gitData));
+  })
+
+  .catch(error => {
+    console.log("Houston we have a problem", error);
+  });
 
 /* Step 4: Pass the data received from Github into your function, 
            create a new component and add it to the DOM as a child of .cards
@@ -24,11 +39,52 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+// Adding instructor cards
+
+const instructors = [
+  "tetondan",
+  "dustinmyers",
+  "justsml",
+  "luishrd",
+  "bigknell"
+];
+
+instructors.forEach(inst => {
+  axios
+    .get(`https://api.github.com/users/${inst}`)
+    .then(response => {
+      const gitData = response.data;
+
+      cardsAttach.appendChild(createUserCard(gitData));
+    })
+
+    .catch(error => {
+      console.log("Something is missing", error);
+    });
+});
+
+// Adding follower cards
+
+axios.get("https://api.github.com/users/Kenneth-Brandon").then(response => {
+  const followersUrl = response.data.followers_url;
+
+  axios
+    .get(followersUrl)
+    .then(res => {
+      const followerArray = res.data;
+
+      followerArray.forEach(item => {
+        cardsAttach.appendChild(createUserCard(item));
+      });
+    })
+
+    .catch(error => {
+      console.log("There is a problem. Here it is: ", error);
+    });
+});
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
-
 <div class="card">
   <img src={image url of user} />
   <div class="card-info">
@@ -43,13 +99,55 @@ const followersArray = [];
     <p>Bio: {users bio}</p>
   </div>
 </div>
-
 */
 
-/* List of LS Instructors Github username's: 
-  tetondan
-  dustinmyers
-  justsml
-  luishrd
-  bigknell
-*/
+function createUserCard(obj) {
+  // Create DOM elements
+
+  const card = document.createElement("div");
+  const userImg = document.createElement("img");
+  const cardInfo = document.createElement("div");
+  const name = document.createElement("h3");
+  const userName = document.createElement("p");
+  const location = document.createElement("p");
+  const profile = document.createElement("p");
+  const profileAddress = document.createElement("a");
+  const followers = document.createElement("p");
+  const following = document.createElement("p");
+  const bio = document.createElement("p");
+
+  // Set classes
+
+  card.classList.add("card");
+  cardInfo.classList.add("card-info");
+  name.classList.add("name");
+  userName.classList.add("username");
+
+  // Set contents / text
+
+  userImg.setAttribute("src", obj.avatar_url);
+  name.textContent = obj.name;
+  userName.textContent = obj.login;
+  location.textContent = obj.location;
+  profile.textContent = "Profile: ";
+  profileAddress.textContent = obj.html_url;
+  profileAddress.setAttribute("href", obj.html_url);
+  followers.textContent = `Followers: ${obj.followers}`;
+  following.textContent = `Following: ${obj.following}`;
+  bio.textContent = `Bio: ${obj.bio}`;
+
+  // Create Structure
+
+  card.appendChild(userImg);
+  card.appendChild(cardInfo);
+  cardInfo.appendChild(name);
+  cardInfo.appendChild(userName);
+  cardInfo.appendChild(location);
+  cardInfo.appendChild(profile);
+  profile.appendChild(profileAddress);
+  cardInfo.appendChild(followers);
+  cardInfo.appendChild(following);
+  cardInfo.appendChild(bio);
+
+  return card;
+}
